67fb080d87dcc8ceb1609029fb9c674ab1604ba0
gussm07 <78830028+gussm07@users.noreply.github.com>
2021-11-23 18:54:19 -0600

    Crear SQLite

--- app/src/main/java/edu/ieu/basedatosartculos/MainActivity.java
+++ app/src/main/java/edu/ieu/basedatosartculos/MainActivity.java
@@ -3,12 +3,36 @@ package edu.ieu.basedatosartculos;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText etCodigo;
+    private EditText etDescripcion;
+    private EditText etPrecio;
+
+    private Button btnNuevo;
+    private Button btnBorrar;
+    private Button btnActualizar;
+
+    private Button btnBuscarCodigo;
+    private Button btnBuscarDescripcion;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etCodigo = findViewById(R.id.etCodigo);
+        etDescripcion = findViewById(R.id.etDescripcion);
+        etPrecio = findViewById(R.id.etPrecio);
+
+        btnNuevo = findViewById(R.id.btnCrear);
+        btnBorrar = findViewById(R.id.btnBorrar);
+        btnActualizar = findViewById(R.id.btnModificar);
+        btnBuscarCodigo = findViewById(R.id.btnBuscarCodigo);
+        btnBuscarDescripcion = findViewById(R.id.btnBuscarDescripcion);
     }
 }
---
+++ app/src/main/java/edu/ieu/basedatosartculos/basedatos/AdminSQLiteOpenHelper.java
@@ -0,0 +1,30 @@
+package edu.ieu.basedatosartculos.basedatos;
+
+import android.content.Context;
+import android.database.ContentObservable;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class AdminSQLiteOpenHelper extends SQLiteOpenHelper {
+    //CONSTANTE SQL
+    private static final String CREATE_TABLE_SQL = "CREATE TABLE ARTICULOS(" +
+            "codigo int primary key," +
+            "descripcion text,"+
+            "precio real);";
+
+    public AdminSQLiteOpenHelper(@Nullable Context context, @Nullable String name){
+        super(context, name, null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        sqLiteDatabase.execSQL(CREATE_TABLE_SQL);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}
