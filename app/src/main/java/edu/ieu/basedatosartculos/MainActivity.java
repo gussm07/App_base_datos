6ba6d901c4c5ab2f5b3cf7460fb5bd81f50987d5
gussm07 <78830028+gussm07@users.noreply.github.com>
2021-11-24 19:07:20 -0600

    Crear SQLite e insercion a base de datos

--- app/src/main/java/edu/ieu/basedatosartculos/MainActivity.java
+++ app/src/main/java/edu/ieu/basedatosartculos/MainActivity.java
@@ -2,11 +2,23 @@ package edu.ieu.basedatosartculos;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import edu.ieu.basedatosartculos.basedatos.AdminSQLiteOpenHelper;
+import static edu.ieu.basedatosartculos.basedatos.AdminSQLiteOpenHelper.BASE_DATOS;
+import static edu.ieu.basedatosartculos.basedatos.AdminSQLiteOpenHelper.CODIGO;
+import static edu.ieu.basedatosartculos.basedatos.AdminSQLiteOpenHelper.DESCRIPCION;
+import static edu.ieu.basedatosartculos.basedatos.AdminSQLiteOpenHelper.PRECIO;
+import static edu.ieu.basedatosartculos.basedatos.AdminSQLiteOpenHelper.TABLA;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     private EditText etCodigo;
     private EditText etDescripcion;
@@ -34,5 +46,41 @@ public class MainActivity extends AppCompatActivity {
         btnActualizar = findViewById(R.id.btnModificar);
         btnBuscarCodigo = findViewById(R.id.btnBuscarCodigo);
         btnBuscarDescripcion = findViewById(R.id.btnBuscarDescripcion);
+
+        btnNuevo.setOnClickListener(view -> {
+            alta();
+        });
+    }
+
+    private void alta() {
+        AdminSQLiteOpenHelper adminDbHelper =
+                new AdminSQLiteOpenHelper(this, BASE_DATOS);
+        SQLiteDatabase adminDb = adminDbHelper.getWritableDatabase();
+        String codigo = etCodigo.getText().toString();
+        String descripcion = etDescripcion.getText().toString();
+        String precio = etPrecio.getText().toString();
+        if(codigo.isEmpty())
+        {
+            Toast.makeText(this, "Codigo no puede ser vacio", Toast.LENGTH_SHORT)
+                    .show();
+            return;
+        }
+        ContentValues values = new ContentValues();
+        values.put(CODIGO, codigo);
+        values.put(DESCRIPCION, descripcion);
+        values.put(PRECIO, precio);
+        adminDb.insert(TABLA, null, values);
+        adminDb.close();
+        Toast.makeText(this, "Artículo con código" +codigo+"guardado en la base de datos", Toast.LENGTH_SHORT).show();
+        limpiarET();
+
+
+
+    }
+
+    private void limpiarET(){
+        etCodigo.setText("");
+        etDescripcion.setText("");
+        etPrecio.setText("");
     }
 }
